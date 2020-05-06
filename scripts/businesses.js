d95a6cc76e53f65f1ce3275c32ebd557ef39b47e
iamkaleb <kalebdmoran@gmail.com>
2020-05-06 14:39:23 -0500

    map() Lightning Exercise

--- scripts/businesses.js
+++ scripts/businesses.js
@@ -156,4 +156,33 @@ manufacturingBusinesses.forEach(manufacturingBusinesses => {
     </section>
   `
   outEl.innerHTML += "<hr/>"
+});
+
+outEl.innerHTML += "<h1>Purchasing Agents</h1>";
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+const agents = businesses.map(business => {
+return {
+    "fullName": business.purchasingAgent.nameFirst + business.purchasingAgent.nameLast,
+    "company": business.companyName,
+    "phoneNumber": business.phoneWork
+    }
 })
+
+console.table(agents)
+
+agents.forEach(agent => {
+  outEl.innerHTML += 
+  `<h2>${agent.fullName}</h2>
+  <section>
+  ${agent.company}
+  </section>
+  <section>
+  ${agent.phoneNumber}
+  </section>
+  `
+  outEl.innerHTML += "<hr/>"
+});
